d4bd841b4b42566a3c9c01d793b55ded322ee229
SumantaKumar Mishra <SumantaKumar.Mishra@csscorp.com>
2019-03-14 16:21:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Main from './Main';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor()
+  {
+    super()
+
+    this.state = {
+      movies : []
+    }
+  }
+
+  onClickHandler()
+  {
+    
+  }
+
+  componentDidMount(){
+    axios.get("https://s3.amazonaws.com/jquerytestcsscorp/movies-in-theaters.json").then((response) => {
+      //console.log(response);
+      this.setState({
+        movies : response.data
+      })
+    })
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="button" value="Click to load JSON" onClick={this.onClickHandler.bind(this)}/>
+        <Main propsVal={10} movies={this.state.movies}/>
       </div>
     );
   }
---
+++ src/Main.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+
+class Main extends Component{
+    
+    constructor(props)
+    {
+        console.log("--------> calling constructor")
+
+        super(props);
+        this.state = {
+            val: 10,
+            movies: [
+                {
+                    id: 0,
+                    title: "Dummy Title"
+                }
+            ]
+        }
+    }
+
+    shouldComponentUpdate(){
+        console.log("--------> calling shouldComponentUpdate");
+        return true;
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log("--------> calling getDerivedStateFromProps")
+        console.log("props in Main", props.movies);
+
+        if(state.movies !== props.movies)
+        {
+            return {
+                movies: props.movies
+            }
+        }
+        return null;
+    }
+
+    componentDidMount(){
+        console.log("--------> calling componentDidMount")
+    }
+
+    componentDidUpdate(){
+        console.log("--------> calling componentDidUpdate")
+    }
+
+    
+
+    render(){
+
+        console.log("--------> calling render: ")
+        const {propsVal} = this.props
+
+        const moviesList = this.state.movies.map((item) => {
+            return <li key={item.id}>{item.title}</li>
+        })
+
+        return(
+            <div>Hello Main Component {this.state.val} and props is {propsVal}
+                <ul>{moviesList}</ul>
+            </div>
+        )
+    }
+}
+
+export default Main;
